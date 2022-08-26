a0ad4753e6862465e1e878be01ef8ac70045ccdf
Carlos Diestro <carlostroller99@gmail.com>
2022-08-26 16:22:17 +0200

    refactor: update time manager

--- app/src/main/java/com/carlosdiestro/levelup/core/ui/TimeManager.kt
+++ app/src/main/java/com/carlosdiestro/levelup/core/ui/TimeManager.kt
@@ -1,25 +1,24 @@
 package com.carlosdiestro.levelup.core.ui
 
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.*
 
-interface TimeManager {
-    fun todayInMillis() : Long
-    fun todayInDate(format: DateFormat) : String
-
+object TimeManager {
     sealed class DateFormat(val value: String) {
         object DDMMYYYY : DateFormat("dd/MM/yyyy")
-        object MMMDD : DateFormat("MMM dd")
-    }
-}
-
-private class TimeManagerImpl() : TimeManager {
-    override fun todayInMillis(): Long {
-        return System.currentTimeMillis()
     }
 
-    override fun todayInDate(format: TimeManager.DateFormat): String {
-        val date = LocalDate.now()
-        return date.format(DateTimeFormatter.ofPattern(format.value))
-    }
+    fun now(): String = LocalDate.now().format(
+        DateTimeFormatter.ofPattern(DateFormat.DDMMYYYY.value)
+    )
+
+    fun yesterday(): String = LocalDate.now().minusDays(1).format(
+        DateTimeFormatter.ofPattern(DateFormat.DDMMYYYY.value)
+    )
+
+    fun toMillis(date: String): Long = SimpleDateFormat(DateFormat.DDMMYYYY.value, Locale.getDefault()).parse(date)!!.time
+
+    fun toText(date: Long) : String = SimpleDateFormat(DateFormat.DDMMYYYY.value, Locale.getDefault()).format(Date(date))
 }
