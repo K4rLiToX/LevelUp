f3dc17fe901b4e618c8c5a050ac4f8516bd12366
Carlos Diestro <carlostroller99@gmail.com>
2022-08-26 16:23:55 +0200

    feat: create db and modelize body weight

---
+++ app/src/main/java/com/carlosdiestro/levelup/bodyweight_progress/framework/BodyWeightDAO.kt
@@ -0,0 +1,20 @@
+package com.carlosdiestro.levelup.bodyweight_progress.framework
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BodyWeightDAO {
+
+    @Insert
+    suspend fun insert(entity: BodyWeightEntity)
+
+    @Update
+    suspend fun update(entity: BodyWeightEntity)
+
+    @Delete
+    suspend fun delete(entity: BodyWeightEntity)
+
+    @Query("SELECT * FROM body_weight_table ORDER BY date ASC")
+    fun getAll(): Flow<List<BodyWeightEntity>?>
+}
