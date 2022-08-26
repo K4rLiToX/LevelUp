308b65aca3661cd1453cc90c6357840331e73b02
Carlos Diestro <carlostroller99@gmail.com>
2022-08-26 16:26:18 +0200

    chore: initial body weight di

---
+++ app/src/main/java/com/carlosdiestro/levelup/core/data/DataModule.kt
@@ -0,0 +1,16 @@
+package com.carlosdiestro.levelup.core.data
+
+import com.carlosdiestro.levelup.bodyweight_progress.data.repositories.BodyWeightRepositoryImpl
+import com.carlosdiestro.levelup.bodyweight_progress.domain.repositories.BodyWeightRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataModule {
+
+    @Binds
+    abstract fun bindBodyWeightRepository(bodyWeightRepositoryImpl: BodyWeightRepositoryImpl) : BodyWeightRepository
+}
---
+++ app/src/main/java/com/carlosdiestro/levelup/core/framework/FrameworkModule.kt
@@ -0,0 +1,35 @@
+package com.carlosdiestro.levelup.core.framework
+
+import android.content.Context
+import androidx.room.Room
+import com.carlosdiestro.levelup.bodyweight_progress.framework.BodyWeightDAO
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FrameworkModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): LevelUpDatabase {
+        return Room.databaseBuilder(
+            context,
+            LevelUpDatabase::class.java,
+            "levelupdb"
+        )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBodyWeightDAO(db: LevelUpDatabase): BodyWeightDAO {
+        return db.bodyWeightDao()
+    }
+}
