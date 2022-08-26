b20e1f73246d678452c3ceff9946d58ef5b44306
Carlos Diestro <carlostroller99@gmail.com>
2022-08-26 16:23:06 +0200

    feat: modelize response for ui

---
+++ app/src/main/java/com/carlosdiestro/levelup/core/domain/Response.kt
@@ -0,0 +1,7 @@
+package com.carlosdiestro.levelup.core.domain
+
+sealed class Response<T>(val data: T? = null, val message: String? = null) {
+    class Loading<T>: Response<T>(null, null)
+    class Success<T>(data: T) : Response<T>(data)
+    class Error<T>(message: String) : Response<T>(null, message)
+}
