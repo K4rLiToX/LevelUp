a96ca7cc7e69d38846de31976e7e009c3fcc00a9
Carlos Diestro <carlostroller99@gmail.com>
2022-08-26 09:00:09 +0200

    refactor: include time types in delegate

--- app/src/main/java/com/carlosdiestro/levelup/core/ui/TimeDelegate.kt
+++ app/src/main/java/com/carlosdiestro/levelup/core/ui/TimeDelegate.kt
@@ -6,6 +6,11 @@ import java.time.format.DateTimeFormatter
 interface TimeDelegate {
     fun todayInMillis() : Long
     fun todayInDate(format: DateFormat) : String
+
+    sealed class DateFormat(val value: String) {
+        object DDMMYYYY : DateFormat("dd/MM/yyyy")
+        object MMMDD : DateFormat("MMM dd")
+    }
 }
 
 private class TimeDelegateImp() : TimeDelegate {
@@ -13,13 +18,8 @@ private class TimeDelegateImp() : TimeDelegate {
         return System.currentTimeMillis()
     }
 
-    override fun todayInDate(format: DateFormat): String {
+    override fun todayInDate(format: TimeDelegate.DateFormat): String {
         val date = LocalDate.now()
         return date.format(DateTimeFormatter.ofPattern(format.value))
     }
-}
-
-sealed class DateFormat(val value: String) {
-    object DDMMYYYY : DateFormat("dd/MM/yyyy")
-    object MMMDD : DateFormat("MMM dd")
 }
